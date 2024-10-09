4b9d3a7e77a9ddca2a6087355ccc10d821baf352
Amanda Soares <amanda.soares@ifood.com.br>
2024-10-09 10:37:23 -0300

    Add extension function

--- app/src/main/java/com/example/propertyanimation/MainActivity.kt
+++ app/src/main/java/com/example/propertyanimation/MainActivity.kt
@@ -63,7 +63,7 @@ class MainActivity : AppCompatActivity() {
     private fun rotater() {
         val animator = ObjectAnimator.ofFloat(star, View.ROTATION, -360f, 0f)
         animator.duration = 1000
-        disableViewDuringAnimation(rotateButton, animator)
+        animator.disableViewDuringAnimation(rotateButton)
         animator.start()
     }
 
@@ -72,13 +72,13 @@ class MainActivity : AppCompatActivity() {
         val animator = ObjectAnimator.ofFloat(star, View.TRANSLATION_X, 200f)
         animator.repeatCount = 1
         animator.repeatMode = ObjectAnimator.REVERSE
-        disableViewDuringAnimation(translateButton, animator)
+        animator.disableViewDuringAnimation(translateButton)
         animator.start()
 
     }
 
-    private fun disableViewDuringAnimation(view: View, animator: ObjectAnimator) {
-        animator.addListener(object : AnimatorListenerAdapter() {
+    private fun ObjectAnimator.disableViewDuringAnimation(view: View) {
+        addListener(object : AnimatorListenerAdapter() {
             override fun onAnimationStart(animation: Animator) {
                 view.isEnabled = false
             }
